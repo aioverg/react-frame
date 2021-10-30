c5d98fd7d071afc368fbd2be30175988a755825f
aioverg <aioverg@outlook.com>
2021-10-30 16:03:16 +0800

    动画

--- src/components/d3/D3Ex/index.tsx
+++ src/components/d3/D3Ex/index.tsx
@@ -24,21 +24,22 @@ interface D3ExProps {
 
 function D3Ex(props: D3ExProps) {
   const svgRef = useRef(null)
-  const alphabet = "1234567890".split("")
+  const divRef = useRef(null)
+  // const alphabet = "ABCDEFGHIGKLMNOPQRSTUVWXYZ".split("")
+  const alphabet = "ABCD".split("")
   const data = [
-    {name: '张三', age: 18},
-    {name: '李四', age: 20},
-    {name: '王五', age: 22}
+    { name: '张三', age: 18 },
+    { name: '李四', age: 20 },
+    { name: '王五', age: 22 }
   ]
 
   const init = () => {
-    
     const x = d3.scaleLinear()
-    .domain([0,9])
+    .domain([0, 9])
     .range([18, 360])
-    d3.select(svgRef.current).style('width', 600).style('height', 300).append('g')
+    d3.select(svgRef.current)
+    .append('g')
     .call(d3.axisBottom(x))
-    // console.log(313123, x.bandwidth())
   }
 
 
@@ -47,9 +48,28 @@ function D3Ex(props: D3ExProps) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [svgRef])
 
+  const enter = () => {
+    const color = ['red', 'black', 'yellow', 'blue']
+    let index = 0
+    let timer:any = null
+    const div = d3.select(divRef.current)
+    timer = d3.interval(
+      () => {
+        div.style('background', color[index])
+        index += 1
+        console.log(99999999999)
+        if(index === 4 ){
+          timer.stop()
+        }
+      }, 1000)
+  }
+
   return (
     <ContentBox>
-      <svg ref={svgRef} ></svg>
+      <button onClick={enter}>enter</button>
+      <svg ref={svgRef}></svg>
+      <div ref={divRef} style={{width: '100px', height: '100px'}}>
+      </div>
     </ContentBox>
   )
 }
