e63626dbf17335dcff21d081f8e5a0db307a0a9f
aioverg <aioverg@outlook.com>
2021-11-24 22:24:15 +0800

    modify file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route  } from 'react-router-dom';
-import Login from './views/Login';
-import Home from './views/Home';
+import Login from './views/login';
+import Home from './views/home';
 
 function App() {
   return (
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,12 @@ const router:any = [
   {
     name: '首页',
     path: '/home',
-    component: React.lazy(() => import('@src/views/Home/Main')),
+    component: React.lazy(() => import('@src/views/home/Main')),
   },
   {
     name: 'D3',
     path: '/d3',
-    component: React.lazy(() => import('@src/views/D3')),
+    component: React.lazy(() => import('@src/views/d3')),
   }
 ]
 
